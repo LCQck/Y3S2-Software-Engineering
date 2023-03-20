40c6c21eb28025766e3363e0eb66334b52367621
unknown <mr.kient020808@gmail.com>
2023-03-20 17:45:13 +0800

    commit successfully

---
+++ src/main/java/cpt202/project/pizzaorederingsys/PizzaOrederingSysApplication.java
@@ -0,0 +1,13 @@
+package cpt202.project.pizzaorederingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrederingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrederingSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cpt202/project/pizzaorederingsys/PizzaOrederingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package cpt202.project.pizzaorederingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrederingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
